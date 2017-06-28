f74c0c4d8ba7ceaff2a9ee76d1e2d132b5f8964f
Damien RAYMOND <dam.ray49@gmail.com>
2017-06-28 16:01:01 +0200

    Add slot works WIP

--- src/app/newapp_pages/planning/planning.controller.js
+++ src/app/newapp_pages/planning/planning.controller.js
@@ -56,6 +56,50 @@
             ]
           };
 
+          function serializingDate() {
+            planning.model.items.forEach(function(item, itemIdx) {
+              if(planning.model.items[itemIdx].schedules) {
+                Object.keys(planning.model.items[itemIdx].schedules).forEach(function(scheduleTypeIdx) {
+                  planning.model.items[itemIdx].schedules[scheduleTypeIdx].forEach(function(schedule, scheduleIdx) {
+                    planning.model.items[itemIdx].schedules[scheduleTypeIdx][scheduleIdx].start = schedule.start.toJSON()
+                    planning.model.items[itemIdx].schedules[scheduleTypeIdx][scheduleIdx].end = schedule.end.toJSON()
+                  });
+                })
+              }
+            });
+          }
+
+          function unserializingDate() {
+            planning.model.items.forEach(function(item, itemIdx) {
+              if(planning.model.items[itemIdx].schedules) {
+                Object.keys(planning.model.items[itemIdx].schedules).forEach(function(scheduleTypeIdx) {
+                  planning.model.items[itemIdx].schedules[scheduleTypeIdx].forEach(function(schedule, scheduleIdx) {
+                    planning.model.items[itemIdx].schedules[scheduleTypeIdx][scheduleIdx].start = new Date(schedule.start)
+                    planning.model.items[itemIdx].schedules[scheduleTypeIdx][scheduleIdx].end = new Date(schedule.end)
+                  });
+                })
+              }
+            })
+          }
+
+          function normalizeSchedule() {
+            planning.model.items.forEach(function(item, idx) {
+              if(!item.schedules) {
+                planning.model.items[idx].schedules = {
+                  predicted: [],
+                  realised: []
+                }
+              }
+              if(!item.schedules.predicted) {
+                planning.model.items[idx].schedules.predicted = []
+              }
+              if(!item.schedules.realised) {
+                planning.model.items[idx].schedules.realised = []
+              }
+            })
+            unserializingDate();
+          }
+
 
           var rootRef = firebase.database().ref();
           var ref = rootRef.child('users');
@@ -63,114 +107,27 @@
           planning.model.items = $firebaseArray(ref);
 
           planning.model.items.$loaded().then(function() {
-
-            planning.model.items.forEach(function(item, idx) {
-              console.log('idx', idx)
-              if(!item.schedules) {
-                planning.model.items[idx].schedules = {
-                  predicted: [],
-                  realised: []
-                }
-              }
-              planning.model.items.$save(idx)
-                .then(function(ref) {
-                  console.log('planning.model.items saved')
-                });
-            })
-
-            /*angular.forEach(planning.model.items, function(item) {
-              if(!item.schedules) {
-                item.schedules = {
-                  predicted: [],
-                  realised: []
-                }
-              }
-            });*/
-            //planning.model.items.$save(planning.model.items);
+            normalizeSchedule();
           })
 
-          /*planning.model.items.$watch(function(){
-            console.log("planning.model.items has changed")
-          })*/
-
-          /*ref.on('value', function(snap) {
-            // var data = snap.val();
-            planning.model.items = snap;
-
-            angular.forEach(planning.model.items, function(item) {
-              if(!item.schedule) {
-                item.schedule = {
-                  predicted: [],
-                  realised: []
-                }
-              }
-            });
-
-
-
-            // var dataWithKeys = Object.keys(data).map(function(key) {
-            //   var obj = data[key];
-            //   obj._key = key;
-            //   return obj;
-            // });
-            // planning.model.items = dataWithKeys.map(function(item) {
-            //   if(!item.schedule) {
-            //     item.schedule = {
-            //       predicted: [],
-            //       realised: []
-            //     }
-            //   }
-            //   return item;
-            // });
-            $scope.$apply();
-          })*/
-
-          //planning.model.items = ref.$asArray();
-
-          //planning.model.items.$bindTo($scope, planning.model.items)
-
-          // ref.on('child_added', function(snap) {
-          //   var user = snap.val();
-          //   if(!user.label) {
-          //     user.label = user.name;
-          //   }
-          //   if(!user.editable) {
-          //     user.editable = true;
-          //   }
-          //   if(!user.schedules) {
-          //     user.schedules = {
-          //       predicted: [],
-          //       realised: []
-          //     };
-          //   }
-          //   //planning.model.items.$bindTo($scope, 'planning.model.items')
-          //   $scope.$apply(function() {
-          //     planning.model.items = planning.model.items.concat([user.schedule]);
-          //   })
-          // })
 
           planning.onSlotAdded = function(cb) {
-
             cb({
               project: 'Project X',
               client: 'Client Y'
             });
-            /*console.log('onSlotAdded')
-            return {
-              project: 'ProjetX',
-              client: 'ClientY'
-            }*/
           }
 
           planning.getSlotText = function(schedule) {
-            console.log('getSlotText', schedule)
             return 'azdazd'
           }
 
           planning.doSomething = function(itemIndex, scheduleIndex, scheduleValue) {
+            serializingDate();
             planning.model.items.$save(itemIndex)
                 .then(function(ref) {
-                  console.log('planning.model.items saved')
+                  normalizeSchedule();
+                  console.log('Saved', planning.model.items[itemIndex])
                 });
           };
 
